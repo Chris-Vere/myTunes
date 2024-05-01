019298365281b1f1611e0af2248840a603b25d14
Chris <chris.vere@me.com>
2024-04-30 22:31:47 -0400

    Converts most components to Tailwind

--- src/components/features/TrackWell/TrackWell.tsx
+++ src/components/features/TrackWell/TrackWell.tsx
@@ -24,7 +24,7 @@ export default function TrackWell(props: TrackWellProps) {
     isLoading: tracksAreLoading,
   } = trackStatus;
   return (
-    <div className="col-start-1 col-end-5 w-[calc(100%+80px)] -mb-5 pt-0 px-10 pb-8 bg-gradient-to-b from-neutral-600 to-neutral-950 bg-[length:100%_60%] bg-no-repeat bg-bottom bg-neutral-600 border-y border-neutral-500 border-b-neutral-800 translate-x-[calc(var(--spacing-album-grid)*-1)] translate-y-[calc(var(--spacing-album-grid)*-0.55)]">
+    <div className="col-start-1 col-end-5 w-[calc(100%+80px)] -mb-5 pt-0 px-10 pb-8 bg-gradient-to-b from-neutral-600 to-neutral-950 bg-[length:100%_60%] bg-no-repeat bg-bottom bg-neutral-600 border-y border-neutral-700 border-b-neutral-800 translate-x-[calc(var(--spacing-album-grid)*-1)] translate-y-[calc(var(--spacing-album-grid)*-0.55)]">
       <TrackWellPointer indicatorPosition={indicatorPosition} />
       <h2>{album.title}</h2>
       <TrackWellList tracks={trackData} />
